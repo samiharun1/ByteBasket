48b014e5c38eb65696643b395dc16a180593d9a9
samih <samih@LAPTOP-UHMUODJD>
2024-10-29 10:42:37 +0100

    Add project files.

---
+++ ByteBasket/Order.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBasket
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string Product { get; set; }
+        public string KundNamn { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime OrderDatum { get; set; }
+
+
+        public Order(int orderid, string Prodct, string kundnamn, decimal totalPrice, DateTime orderdatum) 
+        {
+            OrderId = orderid;
+            Product = Prodct;
+            KundNamn = kundnamn;
+            TotalPrice = totalPrice;
+            OrderDatum = DateTime.Now; // rikigta datum 
+
+        }
+        public void ShowOrderDetaljer()
+        {
+            Console.WriteLine($"Order-ID: {OrderId}");
+            Console.WriteLine($"Produkt: {Product}");
+            Console.WriteLine($"Kund: {KundNamn}");
+            Console.WriteLine($"Total Pris: {TotalPrice:C}");
+            Console.WriteLine($"Orderdatum: {OrderDatum}");
+        }
+    }
+
+}
+
---
+++ ByteBasket/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ByteBasket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Order newOrder = new Order("101", "Dator", "Sami Harun", 9999.99m, DateTime.Now);
+                newOrder.ShowOrderDetaljer();
+            }
+        }
+    }
+}
